365b5cef1527d3bf893b2611ca0c8c6c6d92c211
V-Kwong <vincent_kwong578@hotmail.com>
2016-12-27 10:43:10 -0800

    Users can submit entries with the input text box and submit button. The first entry wil receive a key of 0 and the global variable will then increment so each entry will receive a key one unit greater than the last entry. Users can retrieve the entries by typing the corresponding key in the output key text box and the retrieve button. The clear button will delete all user data.

---
+++ app/src/main/java/com/emergence/sharedpreferencesarraytry/MainActivity.java
@@ -0,0 +1,58 @@
+package com.emergence.sharedpreferencesarraytry;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    public final static String filename = "data"; // define file name for data
+    SharedPreferences sharedpreferences;
+    TextView input;
+    TextView key;
+    TextView output;
+    int array_index = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input = (TextView) findViewById(R.id.input); // initialize input as input edit text box
+        key = (TextView) findViewById(R.id.key); // initialize key as key edit text box
+        sharedpreferences = getSharedPreferences( filename, Context.MODE_PRIVATE); // initialize shared preferences
+    }
+
+    public void submit(View view){
+        input = (TextView) findViewById(R.id.input); // initialize input as input edit text box
+        key = (TextView) findViewById(R.id.key); // initialize key as key edit text box
+        String data = input.getText().toString(); // get string in input edit text box
+        SharedPreferences.Editor editor = sharedpreferences.edit(); // initialize editor
+        editor.putString( Integer.toString(array_index), data); // key is array index and data is input
+        editor.commit(); // commit changes
+        input.setText(""); // clear input edit text box
+        array_index++; // increase array index
+    }
+
+    public void retrieve(View view) {
+        input = (TextView) findViewById(R.id.input); // initialize input as input edit text box
+        key = (TextView) findViewById(R.id.key); // initialize key as key edit text box
+        output = (TextView) findViewById(R.id.output); // initialize output as output text box
+        String output_key = key.getText().toString(); // get string in output key edit text box
+        if (sharedpreferences.contains(output_key)) { // print out data corresponding to key to output text box if it exists
+            output.setText(sharedpreferences.getString(output_key, ""));
+        }
+        else
+            output.setText("Data for the corresponding key does not exist!");
+    }
+
+    public void clear(View view) {
+        SharedPreferences.Editor editor = sharedpreferences.edit(); // initialize editor
+        editor.clear(); // clear all memory
+        editor.commit(); // commit changes
+        output.setText("All entries cleared!"); // display to notify user
+    }
+}
