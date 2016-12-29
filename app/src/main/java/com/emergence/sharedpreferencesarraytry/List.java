bbcfd539f0455f098621b20a68d55a603602402b
V-Kwong <vincent_kwong578@hotmail.com>
2016-12-28 18:09:12 -0800

    Complete List with Delete Buttons
    VERY BUGGY
    - added a delete button per entry, however, buttons are large and not
    ideal
    - only last button works per entry into list page
    - doesn't refresh or auto update list when delete button pressed
    - submitting entries doesn't always show up right away anymore, idk why

--- app/src/main/java/com/emergence/sharedpreferencesarraytry/List.java
+++ app/src/main/java/com/emergence/sharedpreferencesarraytry/List.java
@@ -7,6 +7,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class List extends Activity{
@@ -14,20 +16,62 @@ public class List extends Activity{
     public final static String filename = "data"; // define file name for data
     SharedPreferences sharedpreferences;
     TextView list;
+    LinearLayout LinearLayout;
+    Button deleteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         sharedpreferences = getSharedPreferences( filename, Context.MODE_PRIVATE); // initialize shared preferences
-        list = (TextView) findViewById(R.id.listbox);
+        // list = (TextView) findViewById(R.id.listbox); // list is the text box
+        LinearLayout = (LinearLayout) findViewById(R.id.linearLayout); // LinearLayout refers to the linear layout
 
         int i = 0;
 
         while (sharedpreferences.contains(Integer.toString(i))) {
-            list.append(Integer.toString(i + 1) + ".   " + sharedpreferences.getString(Integer.toString(i),"") + "\n");
+            //list.append(Integer.toString(i + 1) + ".   " + sharedpreferences.getString(Integer.toString(i),""));
+
+            list = new TextView(this); // print out each entry
+            list.setText(Integer.toString(i + 1) + ".   " + sharedpreferences.getString(Integer.toString(i),""));
+            list.setId(i);
+            LinearLayout.addView(list);
+
+            deleteBtn = new Button(this); // print out button for each entry
+            deleteBtn.setText("Delete");
+            deleteBtn.setId(i);
+            LinearLayout.addView(deleteBtn);
+
+            //list.append("\n");
             i++;
         }
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //delete(deleteBtn);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                int last_index = 0;
+
+                while(sharedpreferences.contains(Integer.toString(last_index))) {
+                    last_index++;
+                }
+                last_index--;
+
+                // code to delete selected data and replace it with the data from the last index
+                int delIndex = deleteBtn.getId();
+
+                while(delIndex < last_index) {
+                    editor.putString(Integer.toString(delIndex),sharedpreferences.getString(Integer.toString(delIndex + 1),""));
+                    delIndex++;
+                }
+
+                editor.remove(Integer.toString(last_index));
+                editor.commit();
+                LinearLayout.invalidate();
+                //notifyDataSetChanged();
+            }
+        });
 
     }
 
@@ -41,10 +85,18 @@ public class List extends Activity{
         }
         last_index--;
 
-        // code to change delete data and replace it with the data from the last index
+        // code to delete selected data and replace it with the data from the last index
+        int delIndex = deleteBtn.getId();
+
+        while(delIndex < last_index) {
+            editor.putString(Integer.toString(delIndex),sharedpreferences.getString(Integer.toString(delIndex + 1),""));
+            delIndex++;
+        }
 
         editor.remove(Integer.toString(last_index));
         editor.commit();
+        LinearLayout.invalidate();
+        //notifyDataSetChanged();
     }
 
 }
